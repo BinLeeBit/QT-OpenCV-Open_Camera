44af7e4a36ef1cb2dfaafa6b359cca62db687187
libinbin <bitlbb2014@163.com>
2017-02-26 15:14:18 +0800

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,1 +1,11 @@
+#include "tems.h"
+#include <QtWidgets/QApplication>
 
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	TEMS w;
+	w.setAttribute(Qt::WA_DeleteOnClose);
+	w.show();
+	return a.exec();
+}
